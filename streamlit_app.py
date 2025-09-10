b27f0f0dc5a39ba16b05153dbf45722efc8f6787
MukundDomo <mukund.barot@domo.com>
2025-09-10 15:53:14 +0100

    Update streamlit_app.py
    commenting out 
    streamlit.title('My parents new healthy Diner')

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-streamlit.title('My parents new healthy Diner')
+#streamlit.title('My parents new healthy Diner')
+
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie:cup_with_straw:")
 st.write(
